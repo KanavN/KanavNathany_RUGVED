757f3d48617b7ce5a6875b9eaa240c80a12e4c2a
kanav <kanavnathany0@gmail.com>
2025-09-25 20:17:39 +0530

    Deleted copy files

--- q1.py
+++
@@ -1,9 +0,0 @@
-def triple_and(a,b,c):
-    if a =='True' and b == 'True' and c == 'True':
-        return True
-    else:
-        return False
-x=input("enter True or False: ")
-y=input("enter True or False: ")
-z=input("enter True or False: ")
-print(triple_and(x,y,z))
--- q5.py
+++
@@ -1,9 +0,0 @@
-def fibo(n):
-    if n==1:
-        return 0
-    elif n==2:
-        return 1
-    else:
-        return(fibo(n-1)+fibo(n-2))
-num=int(input("Enter a number: "))
-print(fibo(num))
