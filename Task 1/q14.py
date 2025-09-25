e07dbd1b031cd98d697ec7674b5bffdab108f5dc
kanav <kanavnathany0@gmail.com>
2025-09-25 23:58:37 +0530

    edit q 11

--- Task 1/q14.py
+++ Task 1/q14.py
@@ -2,13 +2,15 @@ def repeat(n):
     arr=[]
     for i in range(n):
         arr.append(int(input("Enter a number : ")))
-    x=n-1
+    flag=0
     y=0
     for i in range(n-1):
         for j in range(i+1,n):
             if arr[i]==arr[j]:
-                if j<x:
-                    x=j
-                    y=arr[i]
-    print(str(y)+ " is the first repeating element and occurs again at index "+str(x))
+                y=arr[i]
+                flag=1
+                break
+        if flag==1:
+            break
+    print(str(y)+ " is the first repeating element ")
 repeat(int(input("Enter size of list : ")))
